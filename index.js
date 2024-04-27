7d2ec4b24b43fe021a95a73aac3a69d6cd105ec4
rasel754 <raselahmed73614@gmail.com>
2024-04-28 03:04:08 +0600

    post data successfully

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const craftCollection = client.db('craftDB').collection('craft');
+
+    app.post('/addCraft', async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+  })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
