b0db57f53f7a880bfb2a3b58548d807545305ef3
rasel754 <raselahmed73614@gmail.com>
2024-04-28 20:50:15 +0600

    successfully do delete operation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -39,7 +39,7 @@ async function run() {
 
   app.get('/myArtAndCraft/:id',async (req , res) => {
     console.log(req.params);
-    const result = await craftCollection.find({ownerEmail:req.params.email}).toArray();
+    const result = await craftCollection.find({ownerEmail: req.params.ownerEmail}).toArray();
     res.send(result);
   })
 
@@ -48,6 +48,13 @@ async function run() {
     const result = await cursor.toArray();
     res.send(result);
   })
+
+  app.delete('/myArtAndCraft/:id', async(req, res) => {
+     const id = req.params.id;
+    const query ={_id: new ObjectId (id)};
+    const result = await craftCollection.deleteOne(query);
+    res.send(result);
+  });
 
 
 
